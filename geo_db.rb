4fd3854d22a91010105e5d38b7002dca0eb1f94a
goldbook <hoge>
2012-07-28 15:37:12 +0900

    諸々変更

--- geo_db.rb
+++ geo_db.rb
@@ -1,6 +1,8 @@
 ﻿require 'pg'
 require 'pry'
 
+puts ARGV
+
 unless ARGV.size == 2
   puts "need username, password"
   puts "user: ARGV[0] ,password: ARGV[1]"
@@ -9,43 +11,48 @@ end
 
 TABLE_NAME_PREFIX = "n03%" #大文字小文字区別する
 DB_NAME = "geo"
-TODOFUKEN_NUM = 47
+TODOFUKEN_NUM = 47 # 都道府県の数
 
 conn = PG.connect(dbname: DB_NAME, user: ARGV[0] ,password: ARGV[1]);
 puts conn
+
+# shp取り込み結果のテーブルじゃないので消しておく
+select_table_name_sql = "select relname from pg_stat_user_tables where relname = $1"
+conn.prepare("select_table_name", select_table_name_sql)
+conn.exec("drop table n03_001") if conn.exec_prepared("select_table_name", ["n03_001"]).count == 1;
+
 sql = "select relname from pg_stat_user_tables where relname like $1 order by relname"
 conn.prepare("pref_tabels", sql)
-conn.describe_prepared("pref_tabels")
 ret = conn.exec_prepared("pref_tabels", [TABLE_NAME_PREFIX])
+
+ret.each{|r| puts r}
 
 unless ret.count > 0
   puts "no shp tables"
   return 0
 end
 
-# 都道府県名の配列
-pref_names = ret.values.map do |n|
+# 各テーブルの都道府県名をテーブルへ入れていく
+insert_pref_sql = "insert into n03_001 (id, n03_001) values (select $1, n03_001)"
+conn.prepare("insert_pref", sql)
+
+conn.exec("begin")
+ret.values.each_with_index do |n, i|
+  # テーブル名を可変にしてprepareはできないので都度生成
   sql = "SELECT distinct n03_001 pref FROM \"#{n[0]}\" where n03_001 is not null"
-  conn.exec(sql)[0]["pref"]
-end
-
-pref_names.each{|pn| puts pn.to_s.encode("cp932")}
-
-# insert_pref_sql = "insert into n03_001 (id, n03_001) values (select $1, n03_001)"
-
-# conn.prepare("insert_pref", sql)
-# conn.exec("begin")
-# pref_names.flatten.each_with_index do |e, i|
-  # puts e["pref"].encode("cp932")
-
-  # if i == 0
-    #ループ初回でテーブル作成
-    # conn.exec("create table prefectures as (select '#{i}' id, #{e} from)")
-	# continue
-  # end
+  pref_name = conn.exec(sql)[0]["pref"]
   
-  # conn.exec_prepared("insert_pref", [])
-# end
+  puts i.to_s + ": " + pref_name.encode("cp932")
+
+  if i == 0
+    #ループ初回でテーブル作成
+    conn.exec("create table n03_001 as (SELECT distinct n03_001 pref FROM \"#{n[0]}\" where n03_001 is not null)")
+	next
+  else
+    #ループ初回以外
+	
+  end  
+end
 
 conn.exec("end")
 
