a51f5a81b7658de0241549e5ee7a0c61b41a88b0
goldbook <hoge>
2012-08-05 02:30:36 +0900

    支庁テーブルのテスト条件とテスト自体のフロー修正

--- watchr.rb
+++ watchr.rb
@@ -57,19 +57,27 @@ watch( '(.*)\.rb' ) do |md|
       result.check(db[:n03_001].where(:name => "北海道").one?,"exist hokkaido?")
       result.check(db[:n03_001].where(:name => "沖縄県").one?,"exist okinawa?")
     end
+    
     result.check(db.table_exists?(:n03_002), "db.table_exists?(:n03_002)")
     result.check(db.table_exists?(:n03_003), "db.table_exists?(:n03_003)")
     result.check(db.table_exists?(:n03_004), "db.table_exists?(:n03_004)")
     result.check(db.table_exists?(:n03_007), "db.table_exists?(:n03_007)")
 
     result.check(
-      db[:n03_002].where(:name.like('%支庁%')).all.count > 0, 
+      db[:n03_002].where(:name.like('%支庁%'), 'parent_id IS NOT NULL').all.count > 0, 
       "exist sityou?"
     )
-
-    puts result.to_s
+    # 北海道以外は支庁名を含まない
+    # "支庁名	当該都道府県が「北海道」の場合、該当する支庁の名称。	文字列型"
+    # http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03.html
+    hokkaido_id = db[:n03_001].select(:id).where(:name => "北海道").first[:id]
+    count_valid_row = db[:n03_002].where(:name.like('%支庁%'), :parent_id=>hokkaido_id).count
+    result.check(count_valid_row > 0, "count valid row")
   rescue
+    p result
     system("watchr watchr.rb #{ARGV[1]} #{ARGV[2]}")
+  ensure
+    p result
   end
 end
 
